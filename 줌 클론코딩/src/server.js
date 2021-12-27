b44b26265b534e9eb1ec73bbae38e35aa2aefff6
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-12-27 16:30:12 +0900

    [ 줌 클론코딩 ] Frontend Setup

--- 줌 클론코딩/src/server.js
+++ 줌 클론코딩/src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 
 const app = express();
+const port = 3000;
 
-console.log("Hello")
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:${port}`)
+
+app.listen(port, handleListen);  
