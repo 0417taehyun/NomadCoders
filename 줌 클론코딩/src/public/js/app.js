3795c7ac05c65f432b2c3751cddb22b14f75145a
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-12-27 17:34:18 +0900

    [ 줌 클론코딩 ] WebSocket Events and Messages

--- 줌 클론코딩/src/public/js/app.js
+++ 줌 클론코딩/src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("Connect Success");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected")
+});
+
+setTimeout(() => {
+    socket.send("Hello from Client");
+}, 1000);
--- 줌 클론코딩/src/server.js
+++ 줌 클론코딩/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebScoket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -8,8 +10,23 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
-app,get("/*", (_, res) => res.redirect("/"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:${port}`)
 
-app.listen(port, handleListen);  
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+
+wss.on("connection", (socket) => {
+    console.log("Connected");
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    });
+    socket.on("close", () => {
+        console.log("Disconnected")
+    })
+    socket.send("Hello!");
+});
+
+server.listen(port, handleListen);
